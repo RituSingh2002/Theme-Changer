d94fcb3c4c01bc9d49c48d57a6cc3b3e16103aed
Ritu Singh <69470113+RituSingh2002@users.noreply.github.com>
2022-07-25 18:13:27 +0530

    Theme changer

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let a=document.getElementById("blue");
 let b=document.getElementById("black");
 let c=document.getElementById("green");
 let d=document.getElementById("red");
+let col= document.querySelector("p");
 let area=document.getElementById('message');
 a.addEventListener('click',Blue);
 c.addEventListener('click',Green);
@@ -11,19 +12,24 @@ function Blue(){
    area.style.backgroundColor="blue";
    area.style.color="white";
    area.style.border="solid 5px white"
+   document.getElementById("p").style.backgroundColor ="blue";
 }
 function Black(){
     area.style.backgroundColor="black";
     area.style.color="white";
     area.style.border="solid 5px white"
+    document.getElementById("p").style.backgroundColor ="black";
+
  }
  function Green(){
     area.style.backgroundColor="green";
     area.style.color="black";
     area.style.border="solid 5px black"
+    document.getElementById("p").style.backgroundColor ="green";
  }
  function Red(){
     area.style.backgroundColor="red";
     area.style.color="white";
     area.style.border="solid 5px black"
+    document.getElementById("p").style.backgroundColor ="red";
  }
